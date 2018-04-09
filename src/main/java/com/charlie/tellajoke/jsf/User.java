fee6d96c77df4351e21e2cf8b0ec9554784563e1
Charlie <kikemarroquin@gmail.com>
2018-04-08 21:57:09 -0600

    implements ajax

--- src/main/java/com/charlie/tellajoke/jsf/User.java
+++ src/main/java/com/charlie/tellajoke/jsf/User.java
@@ -7,20 +7,65 @@ package com.charlie.tellajoke.jsf;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import java.io.Serializable;
 
 /**
  *
  * @author charlie
  */
-
 @Named
 @RequestScoped
-public class User {
-    
+public class User implements Serializable {
+
     private String username;
     private String email;
     private String password1;
     private String password2;
+    private String usernameMessage;
+    private final String[] registeredUsernames;
+
+    public User() {
+        registeredUsernames = new String[10];
+        registeredUsernames[0] = "charlie";
+        registeredUsernames[1] = "charlie8";
+        registeredUsernames[2] = "kike8";
+        registeredUsernames[3] = "marroq";
+        registeredUsernames[4] = "marroq8";
+        registeredUsernames[5] = "developer";
+        registeredUsernames[6] = "developer8";
+        registeredUsernames[7] = "coder8";
+        registeredUsernames[8] = "coder";
+        registeredUsernames[9] = "regularuser";
+        username = "";
+    }
+
+    public String getUsernameMessage() {
+        usernameMessage = "that is OK";
+        System.out.println("username.length is: " + username.length());
+        if (username.isEmpty()) {
+            usernameMessage = "";
+        } else if (username.length() < 3) {
+            usernameMessage = "too short";
+        } else if (alreadyExists(username)) {
+            usernameMessage = "already exists";
+        }
+        return usernameMessage;
+    }
+
+    public boolean alreadyExists(String username) {
+        boolean contains = false;
+        for (String registeredUsername : registeredUsernames) {
+            if (registeredUsername.equals(username)) {
+                contains = true;
+                return contains;
+            }
+        }
+        return contains;
+    }
+
+    public void setUsernameMessage(String usernameMessage) {
+        this.usernameMessage = usernameMessage;
+    }
 
     public String getPassword1() {
         return password1;
@@ -54,8 +99,4 @@ public class User {
         this.email = email;
     }
 
-
-    
-    
-    
 }
